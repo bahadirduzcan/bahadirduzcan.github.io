60ec67ab532675e1c1dc9c27f6b37a8a9971f191
bahax41 <bahax41@gmail.com>
2020-05-25 18:08:46 +0300

    Update api.js

--- api.js
+++ api.js
@@ -2,5 +2,5 @@
 "name": "AutoUpdater",
 "version": "1.0.0.5",
 "descriptions": "* Bug fixes and improvements.\r\n * Added Dark Theme.",
-"downloadUrl": "https://github.com/M...utoUpdater.zip"
+"downloadUrl": "https://bahax41.github.io/update.zip"
 }
